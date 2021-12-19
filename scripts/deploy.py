48061aa474bf8e54b16cc3ef2f70ca1e12b0dc6a
NibbledBit <me@nibbledbit.co.uk>
2021-12-19 23:15:01 +0000

    followed the tutorial some more

---
+++ scripts/deploy.py
@@ -0,0 +1,14 @@
+from brownie import accounts, config
+
+
+def deploy_simple_storage():
+    account = accounts[0]
+    print(account)
+    # account = accounts.load("shauns-account")
+    # print(account)
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
+
+
+def main():
+    deploy_simple_storage()
